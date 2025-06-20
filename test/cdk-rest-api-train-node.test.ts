da06472f50e23bcfb8e18fdabc4fb73d002a13d3
yishai <yishai5555@gmail.com>
2025-06-20 16:50:02 +0300

    Initial commit

---
+++ test/cdk-rest-api-train-node.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkRestApiTrainNode from '../lib/cdk-rest-api-train-node-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-rest-api-train-node-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkRestApiTrainNode.CdkRestApiTrainNodeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
