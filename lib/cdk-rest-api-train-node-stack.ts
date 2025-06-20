7ff3e7289a0b1439c34bf162c1ce18847312c335
yishai <yishai5555@gmail.com>
2025-06-20 17:58:12 +0300

    08-קמשנךק-בםרד-שמג-שגג-אירםאאךק

--- lib/cdk-rest-api-train-node-stack.ts
+++ lib/cdk-rest-api-train-node-stack.ts
@@ -23,6 +23,59 @@ export class CdkRestApiTrainNodeStack extends cdk.Stack {
 
     mainPath.addMethod('GET', new apigateway.LambdaIntegration(getQuotes));
 
+    // Enable CORS for the /quotes resource
+    mainPath.addMethod('OPTIONS', new apigateway.MockIntegration({
+      integrationResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        'method.response.header.Access-Control-Allow-Origin': "'*'",
+        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET'"
+      },
+      }],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+      "application/json": "{\"statusCode\": 200}"
+      }
+    }), {
+      methodResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': true,
+        'method.response.header.Access-Control-Allow-Methods': true,
+        'method.response.header.Access-Control-Allow-Origin': true,
+      }
+      }]
+    });
+
+    // Add usage plan and API key for rate limiting
+    const plan = api.addUsagePlan('UsagePlan', {
+      name: 'QuoteApiUsagePlan',
+      throttle: {
+      rateLimit: 100 / 3600, // ~0.0277 req/sec = 100 req/hour
+      burstLimit: 5,
+      },
+      quota: {
+      limit: 100,
+      period: apigateway.Period.HOUR,
+      },
+    });
+
+    const apiKey = api.addApiKey('ApiKey');
+    plan.addApiKey(apiKey);
+    plan.addApiStage({
+      stage: api.deploymentStage,
+      throttle: [
+      {
+        method: mainPath.methods.find(m => m.httpMethod === 'GET'),
+        throttle: {
+        rateLimit: 100 / 3600,
+        burstLimit: 5,
+        },
+      },
+      ],
+    });
+
     //    mainPath.addMethod('GET', new apigateway.LambdaIntegration(getQuotes), {
     //   methodResponses: [{ statusCode: '200' }],
     // });
