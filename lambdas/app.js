295929cc313321cfec53c24574284f2e201b4b4f
yishai <yishai5555@gmail.com>
2025-06-27 16:54:58 +0300

    16-add-save-quote-function

--- lambdas/app.js
+++ lambdas/app.js
@@ -8,12 +8,41 @@ const TABLE_NAME = process.env.TABLE_NAME;
 
 exports.handler = async (event) => {
   console.log('Event::::', JSON.stringify(event, null, 2));
+  console.log('Event::::', event);
 
   return sendResponse(
     200,
     JSON.stringify({ message: 'Hello from the Lambda function!', TABLE_NAME })
   );
 };
+
+async function saveQuote(data) {
+  const time = new Date().getTime();
+  const { quote, by } = data;
+
+  const newQuote = {
+    id: time.toString(),
+    quote,
+    by,
+  };
+
+  const params = {
+    TableName: TABLE_NAME,
+    Item: newQuote,
+  };
+
+  // return dynamo
+  //   .put(params)
+  //   .promise()
+  //   .then(() => {
+  //     return item;
+  return newQuote;
+  //   });
+
+  return docClient.put(params).then(() => {
+    return item;
+  });
+}
 
 const sendResponse = (status, body) => {
   return {
